6f11b64ecaeab84a41287d4654e4446d26514c35
Dhanush123 <dhanush.patel@ymail.com>
2016-05-08 14:37:58 -0700

    sadsadasd
    sdasdasdsad

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,36 @@ var router = express.Router();
 var watson = require('watson-developer-cloud');
 var fs = require('fs');
 var imgs = [];
-var params = {};
+var params = {"images_file": ""};
 var request = require('request').defaults({ encoding: null });
+var Firebase = require('firebase');
+var rootRef = new Firebase("http://amber-torch-1365.firebaseio.com");
+
+var imgArrayLength;
 /* GET home page. */
 router.post('/', function(req, res, next) {
     var imgArray = req.body["imgArray[]"];
+    imgArrayLength = imgArray.length;
     var i = 0;
     //var array = req.params.
     for(var x in imgArray ){
-        request.get(imgArray[x], function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            params.images_file = body;
-            doWatsonImg(imgArray[x])
-        }
-});
-
+        console.log(imgArray[x]);
+        if (imgArray[x][0] == "h"){
+        request.get(imgArray[x])
+            .on('response', function(response){
+            
+        })
+            .pipe(fs.createWriteStream('image.jpg').on('error', function(err){conosle.log('error');}))
+            .on('close', 
+          function(){
+            if (fs.createReadStream('image.jpg'))
+                params.images_file = fs.createReadStream('image.jpg');
+            doWatsonImg(imgArray[x]);
+          });
+    }
 }
-console.log(imgs);
+
+console.log(imgs[0]);
 res.json({images: imgs});  
 
 });
@@ -44,10 +57,17 @@ visual_recognition.classify(params, function(err, res) {
     console.log(err);
   else{
     imgString = JSON.stringify(res, null, 2); 
-    console.log(imgString);
-          imgs.push(imgString);
-  }
 
+    imgs.push(res.images[0].scores[0].name);
+          console.log(imgs);
+  }
+  if (imgs.length){console.log("images");
+    imgs = imgs.filter(function(elem, pos){return imgs.indexOf(elem) == pos;});
+    var imageString = "" + imgs[0];
+    for (var i = 1; i < imgs.length; i++) {
+        imageString += " and " + imgs[i];
+    }
+    rootRef.set({command: "images", images: imageString});}
 });
 
 }
